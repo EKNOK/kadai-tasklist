b344cb915d15519f7abf8a096334608f9758a46b
Eiki Kanaoka <kakakaeee@gmail.com>
2017-10-23 10:06:32 +0000

    修正 heroku error

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
     before_action :set_task, only: [:show, :edit, :update, :destroy]
-    before_action :correct_user, only: [:destroy, :show, :create, :update]
+    before_action :correct_user, only: [:destroy, :show, :update]
     
     
     def index
@@ -44,7 +44,7 @@ class TasksController < ApplicationController
     def destroy
         @task.destroy
         flash[:success] = 'Taskは削除されました'
-        redirect_back(fallback_location: root_url)
+        redirect_to root_url
     end
     
     def set_task
